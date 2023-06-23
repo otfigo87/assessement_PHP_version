c0fcd5897e67ff9f6bdff7fcf16d90ebfd60c419
Otmane Aatik <aaotmane@gmail.com>
2023-06-22 23:20:57 -0400

    time in 12H format

--- script.js
+++ script.js
@@ -33,9 +33,10 @@ setInterval(() => {
   const date = time.getDate();
   const day = time.getDay();
   const hour = time.getHours();
+  const hour12 = hour % 12 || 12;// 12H format
   const minutes = time.getMinutes();
   const ampm = hour >= 12 ? "PM" : "AM";
-  const currentTime = hour + ":" + (minutes < 10 ? "0" + minutes : minutes);
+  const currentTime = (hour12 < 10 ? "0" + hour12 : hour12) + ":" + (minutes < 10 ? "0" + minutes : minutes);
   //console.log(date);
   // console.log(currentTime);
   timeEl.innerHTML = currentTime + `<span id="am-pm">${ampm}</span>`; //span to get same styles
